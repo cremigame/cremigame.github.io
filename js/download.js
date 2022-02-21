bb43ac26b323970faa5bb261aefa6c97f729d881
cremigame <68150073+cremigame@users.noreply.github.com>
2022-02-22 00:00:42 +0200

    Add files via upload

---
+++ js/download.js
@@ -0,0 +1,8 @@
+$(".DownloadSnxw").click(
+    function(e){
+        var a = document.createElement('a');
+        a.href = "https://github.com/SxnwDev/Exploit/raw/main/Snxw%20Boostrapper.exe";
+        a.download = "download";
+        a.click();
+    }
+);
---
+++ js/loader.js
@@ -0,0 +1,3 @@
+window.addEventListener("load", function(){
+    document.getElementById("openload").classList.toggle("endload");
+})
